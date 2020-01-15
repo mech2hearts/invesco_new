d1801d4f50cefe1bd6b26f7a42ae34602246f9dd
Chang Kim <ckim52@student.gsu.edu>
2020-01-15 10:05:20 -0500

    api get call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  state = {}
+
+  componentDidMount() {
+    axios.get(`https://financialmodelingprep.com/api/v3/financials/income-statement/AAPL?period=quarter`)
+    .then(res =>{
+      this.setState({
+        apple: res.data.financials
+      }, ()=>{
+        console.log(res.data)
+      })
+    })
+  }
+  
+
+  render() {
+    const tickers = ['AAPL', 'MSFT', 'AMZN', 'FB', 'GOOGL', 'UBER', 'INTC', 'CMCSA', 'CSCO', 'WORK']
 
     
 
+  return (
     <div className="App">
+      
       
     </div>
   );
+  }
 }
 
 export default App;
